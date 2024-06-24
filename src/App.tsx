fc5d7a7ad12164cdd23df08dcf365eefab540e74
SerafimGalinschii <serafim.galinschii@gmail.com>
2024-06-24 19:51:57 +0300

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (items) => {
-    console.log(items);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
